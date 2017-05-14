16b7bbf7598fa32b892d7fbcb4cf77e9d9d521c5
Nathan Gould <nathan.gould@gmail.com>
2017-05-14 00:17:10 -0400

    delete articles mgmt command

---
+++ articles/management/commands/delete_articles.py
@@ -0,0 +1,14 @@
+import random
+
+from django.core.management.base import BaseCommand, CommandError
+from goose import Goose
+import requests
+
+from articles.models import Article
+
+class Command(BaseCommand):
+  help = 'Closes the specified poll for voting'
+
+  def handle(self, *args, **options):
+    Article.objects.all().delete()
+    self.stdout.write(self.style.SUCCESS("Deleted all articles.")
