3bc2b12b19485cc16e0909a7ad84ccecb58c9c8c
Dan Robinson <danrobinson010@gmail.com>
2017-05-13 23:23:17 -0400

    update database configuration

--- taggernews/taggernews/settings.py
+++ taggernews/taggernews/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
