7da1f163cda0d1a6d61698dc123bef57cb2a98ef
Dan Robinson <danrobinson010@gmail.com>
2017-05-14 03:41:09 -0400

    change key

--- taggernews/settings.py
+++ taggernews/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ci@hatqo-ru^xvw%vitf^jw!@k)n4c76$t+azr-t3ddj+p648'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "True"
